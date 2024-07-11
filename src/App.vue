7354910912d540fa9c453dc74960de5cde2721f3
фарваев азат талгатович <farvaev-at@it-resource.team>
2024-07-12 02:52:10 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <constructorHead />
+  <div className="ask-qustion container"><a href="#">Часто задаваемы вопросы</a></div>
+  <ConstructorListContent />
+
+
+</template>
+<script>
+import { defineComponent } from 'vue'
+import ConstructorHead from './components/constructorHead.vue'
+import ConstructorListContent from './components/constructorListContent.vue'
+export default defineComponent({
+  components: {
+    ConstructorHead,
+    ConstructorListContent
+  }
+})
+</script>
+<style>
+.ask-qustion {
+  padding: 20px 0px;
+
+}
+
+.ask-qustion a {
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 19.2px;
+  color: #0578D6;
+
+}
+</style>
---
+++ src/components/constructorListContent.vue
@@ -0,0 +1,120 @@
+<template>
+    <div className="container">
+        <div class="white-bg">
+            <div class="content-title">
+                <p>Какой формат ИУЛ вы хотите получить?</p>
+                <img src="/src/assets/img/result.png" alt="" srcset="">
+            </div>
+            <div class="content">
+                <div class="conten-body">
+                    <div class="content-item">
+                        <div class="outline">
+                            <div class="grey-bg"></div>
+                        </div>
+
+                    </div>
+                    <div class="outline-button">
+                        Выбрать
+                    </div>
+                </div>
+                <div class="conten-body">
+                    <div class="content-item">
+                        <div class="outline">
+                            <div class="grey-bg"></div>
+                        </div>
+
+                    </div>
+                    <div class="outline-button">
+                        Выбрать
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'constructorListContent',
+
+})
+</script>
+<style>
+.white-bg {
+    background: #FFFFFF;
+    border-radius: 4px;
+    padding: 50px 30px
+}
+
+.content-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.content-title p {
+    font-family: Roboto;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 21.09px;
+    text-align: left;
+}
+
+.content {
+    display: flex;
+
+    gap: 20px;
+}
+
+.content-item {
+    border: 1px solid #E9E9E9;
+    padding: 20px;
+
+}
+
+.grey-bg {
+    background: #E9E9E9;
+    width: 210px;
+    height: 260px;
+    border-radius: 4px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+}
+
+.grey-bg::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+
+    background: url(../assets/img/magnifier.png) no-repeat center center;
+    background-size: contain;
+    width: 32px;
+    height: 32px;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+}
+
+.grey-bg:hover::before {
+    opacity: 1;
+}
+
+.outline-button {
+    margin-top: 20px;
+    padding: 18px 91px;
+    border: 1.5px solid #0578D6;
+    color: #0578D6;
+    border-radius: 4px;
+    cursor: pointer;
+
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 18.75px;
+
+}
+</style>
