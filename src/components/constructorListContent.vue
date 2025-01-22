b667d617bd37fe145e1135f0db9377f70dc4f68d
Азат Фарваев <afarvaev4@gmail.com>
2025-01-22 13:12:17 +0300

    fix styles

--- src/components/constructorListContent.vue
+++ src/components/constructorListContent.vue
@@ -1,60 +1,40 @@
 <template>
-  <div v-if="loading"
-       class="spinner-container"
-  >
+  <div v-if="loading" class="spinner-container">
     <div class="container-spinner">
       <ProgressSpinner />
     </div>
   </div>
-  <div v-else
-       className="container"
-  >
+  <div v-else className="container">
     <div class="white-bg">
       <div class="content-title">
         <p>Какой формат ИУЛ вы хотите получить?</p>
-        <img src="/src/assets/img/result.png"
-             alt=""
-             srcset=""
-        >
+        <img src="/src/assets/img/result.png" alt="" srcset="">
       </div>
 
 
       <div class="content">
-        <div class="conten-body"
-             v-for="item in formattedItems"
-             :key="item.id"
-        >
+        <div class="conten-body" v-for="item in formattedItems" :key="item.id">
           <div class="content-item"
-               :class="{ 'selected-item': item.id === store.state.selectedItems.DOCUMENT_TYPE_ID }"
-          >
+            :class="{ 'selected-item': item.id === store.state.selectedItems.DOCUMENT_TYPE_ID }">
             <div class="outline">
-              <Image preview
-                     alt="iul"
-              >
+              <Image preview alt="iul">
                 <template #previewicon>
                   <img src="/src/assets/img/magnifier.png" />
                 </template>
                 <template #image>
-                  <img class="iul-imges"
-                       :src="`https://services.rsexpertiza.ru${item.img}`"
-                       alt="img"
-                  />
+                  <img class="iul-imges" :src="`https://services.rsexpertiza.ru${item.img}`" alt="img" />
                 </template>
                 <template #preview="slotProps">
-                  <img width="570"
-                       height="650"
-                       :src="`https://services.rsexpertiza.ru${item.img_detail}`"
-                       alt="preview"
-                       :style="slotProps.style"
-                       @click="slotProps.onClick"
-                  />
+                  <img width="570" height="650" :src="`https://services.rsexpertiza.ru${item.img_detail}`" alt="preview"
+                    :style="slotProps.style" @click="slotProps.onClick" />
                 </template>
               </Image>
             </div>
           </div>
-          <div @click="selectItem(item)"
-               class="outline-button"
-          >
+          <div class="iul-name">
+            <p>{{ item.name }}</p>
+          </div>
+          <div @click="selectItem(item)" class="outline-button">
             Выбрать
           </div>
         </div>
@@ -64,10 +44,10 @@
   </div>
 </template>
 <script lang="ts">
-import {defineComponent, ref, onMounted, computed} from 'vue';
+import { defineComponent, ref, onMounted, computed } from 'vue';
 import ProgressSpinner from 'primevue/progressspinner';
 import Image from 'primevue/image';
-import {useStore} from 'vuex';
+import { useStore } from 'vuex';
 
 interface StepData {
   elements?: {
@@ -79,6 +59,7 @@ interface FormattedItem {
   id: string | number;
   img: string;
   img_detail: string;
+  name: string;
 }
 
 export default defineComponent({
@@ -122,7 +103,7 @@ export default defineComponent({
       if (totalItems >= 2 && item.id === this.formattedItems[1].id) {
         this.store.commit('setFlagId', item.id);
       }
-      this.store.commit('addSelectedItem', {DOCUMENT_TYPE_ID: item.id,});
+      this.store.commit('addSelectedItem', { DOCUMENT_TYPE_ID: item.id, });
       this.goToStep1();
     },
     goToStep1() {
@@ -144,8 +125,8 @@ export default defineComponent({
 
 .iul-imges {
   object-fit: cover;
-  width: 210px !important;
-  height: 260px !important;
+  width: 260px !important;
+  height: 242px !important;
 }
 
 .white-bg {
@@ -169,17 +150,39 @@ export default defineComponent({
 
 .content {
   display: flex;
-
-  gap: 20px;
+  gap: 80px;
 }
 
 .content-item {
   border: 1px solid #E9E9E9;
   padding: 20px;
   border-radius: 4px;
+  transition: border-color 0.3s ease;
 
 }
 
+.content-item:hover {
+  border: 1px solid #0578D6;
+}
+
+.content-item:hover .p-image-preview-mask {
+  opacity: 1 !important;
+  cursor: pointer !important;
+}
+
+.p-image-preview:hover>.p-image-preview-mask {
+  opacity: 1;
+  cursor: pointer;
+  background: none !important;
+}
+
+.iul-name p {
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 19.2px;
+  text-align: center;
+  padding-top: 20px;
+}
 
 .outline-button {
   margin-top: 20px;
@@ -188,7 +191,7 @@ export default defineComponent({
   color: #0578D6;
   border-radius: 4px;
   cursor: pointer;
-
+  text-align: center;
   font-size: 16px;
   font-weight: 700;
   line-height: 18.75px;
